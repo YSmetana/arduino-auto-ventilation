3ace7266495644b28a59e87e39b8f6303fae5034
Yuriy <ysmetana@gmail.com>
2018-08-26 09:56:49 +0300

    Screen saver with proximity sensor

--- main-node/main-node.ino
+++ main-node/main-node.ino
@@ -24,9 +24,14 @@
  * Other settings
  */
 #define BLINK_DUR 500UL             // when blinking icons on the dispay
+#define DISPLAY_SAVER 0.2 *60*1000  // minutes, display goes off after this time
+#define PROXIMITY_PIN 2
+bool Display_On = true;             // display state
 bool Receiving = false;             // data receiving status
 byte Receiving_From_Node;           // last data received from this node
 bool Fan_On = false;                // fan is working
+
+
 
 /*
  * Timers
@@ -38,6 +43,7 @@ unsigned long Timer_Check_Vent;               // to check if its is time to chec
 unsigned long Timer_Vent_Running = 0;         // how long is it running already
 unsigned long Timer_Node_Outside = millis();  // node timeout
 unsigned long Timer_Node_Basement = millis(); // node timeout
+unsigned long Timer_Display_On = millis(); // node timeout
 
 /*
  * Data structures
@@ -70,7 +76,7 @@ SENSORS SENS_BASEMENT;  // data from basement sensors
 #define PJON_PACKET_MAX_LENGTH 32
 #define TS_MAX_ATTEMPTS   1
 #define PJON_INCLUDE_TS
-#include <PJON.h>                     
+#include <PJON.h>
 PJON<ThroughSerial> bus(NODE_ID_ME);  // <Strategy name> bus(selected device id)
 
 /*
@@ -82,7 +88,7 @@ PJON<ThroughSerial> bus(NODE_ID_ME);  // <Strategy name> bus(selected device id)
 //U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // full buffer
 // For proper text align.
-#define DISPLAY_WIDTH 128 
+#define DISPLAY_WIDTH 128
 #define DISPLAY_HEIGH 64
 
 /*
@@ -98,18 +104,18 @@ AltSoftSerial HC12;         // Arduino Pro Mini: HC-12_TX-PIN8_RX, HC-12_RX-PIN9
 /*
  * Functions
  */
-void pageTemperature(); 
-void pageHumidity();    
-void pageAbsHumidity();    
-void pagePressure();    
-void pageTimeDay();     
+void pageTemperature();
+void pageHumidity();
+void pageAbsHumidity();
+void pagePressure();
+void pageTimeDay();
 void blink_fan();
 void float2char (float value, char * input, bool sign=false);
 unsigned int center_x(char* message, int margin_left=0, int margin_right=0);
 unsigned int right_x(char* message, int margin_right=0);
 bool run_ventilation();
 
-/* 
+/*
  * Pages on display
  */
 int current_page = 0;
@@ -117,7 +123,7 @@ void (*pages[])() = {pageTemperature, pageHumidity, pageAbsHumidity, pagePressur
 unsigned int pages_count = 4;
 int page_duration[] = {3000UL, 3000UL, 3000UL, 3000UL, 1000UL};
 
-/* 
+/*
  * SETUP
  */
 void setup() {
@@ -134,10 +140,12 @@ void setup() {
   Serial.print("Abs hum. delta is "); Serial.print(HUM_DELTA); Serial.println(" g/m3.");
   Serial.println();
 
+  pinMode(PROXIMITY_PIN, INPUT);
+
   // Display
-  u8g2.begin();  
+  u8g2.begin();
   u8g2.setColorIndex(1);
-  u8g2.setFontMode(0);    // enable transparent mode, which is faster  
+  u8g2.setFontMode(0);    // enable transparent mode, which is faster
 
   // Wireless
   HC12.begin(HC12_SPEED);       // Set HC12 baudrate (you must use the one configured in HC12, default 9600)
@@ -160,13 +168,13 @@ void setup() {
   bus.send_repeatedly(NODE_ID_BASEMENT, (const char*)&Command, sizeof(CONTROL), Node_Request); // seconds * 1000000
   delay(1000);
 
-  Timer_Check_Vent = millis(); // to check if its is time to calc ventilation process  
+  Timer_Check_Vent = millis(); // to check if its is time to calc ventilation process
 };
 
 void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
   /*
    * Pjon errors handlers
-   */  
+   */
   if(code == PJON_CONNECTION_LOST) {
     Serial.print("Conn. with ID ");
     Serial.print(bus.packets[data].content[0], DEC);
@@ -187,16 +195,16 @@ void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
 void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info &packet_info) {
   /*
    * If data are being receiving
-   */  
+   */
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   Timer_Receiving = millis();
   Receiving = true;
   Receiving_From_Node = packet_info.sender_id;
-    
+
   Serial.print("Rec. from ID ");
   Serial.println(packet_info.sender_id);
   Serial.print("Cont. len. ");
-  Serial.println(length);  
+  Serial.println(length);
 
   String content = "";
   char character;
@@ -214,35 +222,35 @@ void receiver_function(uint8_t *payload, uint16_t length, const PJON_Packet_Info
     Timer_Node_Outside = millis();  // update timeout timer
 
     SENS_OUTSIDE = Sensors;         // assign received values to global struct
-    
+
     Serial.print("Outside temp: ");
-    Serial.println(SENS_OUTSIDE.temperature);  
+    Serial.println(SENS_OUTSIDE.temperature);
     Serial.print("Outside hum: ");
-    Serial.println(SENS_OUTSIDE.humidity);  
+    Serial.println(SENS_OUTSIDE.humidity);
     Serial.print("Outside press: ");
-    Serial.println(SENS_OUTSIDE.pressure);    
-    Serial.println();    
-  }  
+    Serial.println(SENS_OUTSIDE.pressure);
+    Serial.println();
+  }
 
   // Basement module
   if (packet_info.sender_id == NODE_ID_BASEMENT) {
     Timer_Node_Basement = millis(); // update timeout timer
 
     SENS_BASEMENT = Sensors;        // assign received values to global struct
-    
+
     Serial.print("Basement temp: ");
     Serial.println(SENS_BASEMENT.temperature);
     Serial.print("Basement hum: ");
-    Serial.println(SENS_BASEMENT.humidity);  
+    Serial.println(SENS_BASEMENT.humidity);
     Serial.print("Basement press: ");
-    Serial.println(SENS_BASEMENT.pressure);    
-    Serial.println();    
-  } 
+    Serial.println(SENS_BASEMENT.pressure);
+    Serial.println();
+  }
 
   digitalWrite(LED_BUILTIN, LOW);    // end of receiving
 };
 
-float rel2abs_hum(float t, float h) {  
+float rel2abs_hum(float t, float h) {
   /*
    * Rel humidity to abs. humidity
    * https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
@@ -256,7 +264,7 @@ float rel2abs_hum(float t, float h) {
     }
 
     return res;
-}  
+}
 
 bool time_to_check_vent () {
   /*
@@ -264,7 +272,7 @@ bool time_to_check_vent () {
    */
     if (millis()-Timer_Check_Vent >= Vent_Check_Every) {
       Timer_Check_Vent = millis();  // reset counter
-      return true;      
+      return true;
     } else {
       return false;
   }
@@ -272,7 +280,7 @@ bool time_to_check_vent () {
 
 bool vent_too_long() {
   if (millis()-Timer_Vent_Running > Vent_Running_Max_Time) {
-    //return true;    
+    //return true;
   } else {
     return false;
   }
@@ -294,12 +302,12 @@ bool run_ventilation() {
   if (!Node_Basement_Active()) {
     Serial.println("Basement module data are not up to date.");
     run = false;
-  }  
+  }
 
   if (vent_too_long()) {
     Serial.println("Ventilator has been working too long already.");  // TODO: power off period
     run = false;
-  }  
+  }
 
   if (SENS_OUTSIDE.humidity == 0 || SENS_BASEMENT.humidity == 0) {
     Serial.println("Outside or basement humidity has wrong value (0).");
@@ -309,11 +317,11 @@ bool run_ventilation() {
     Serial.println("Outside or basement temperature has wrong value (-99.9).");
   }
 
-  if(rel2abs_hum(SENS_OUTSIDE.humidity, SENS_OUTSIDE.temperature) - 
-     rel2abs_hum(SENS_BASEMENT.humidity, SENS_BASEMENT.temperature) 
+  if(rel2abs_hum(SENS_OUTSIDE.humidity, SENS_OUTSIDE.temperature) -
+     rel2abs_hum(SENS_BASEMENT.humidity, SENS_BASEMENT.temperature)
      >= HUM_DELTA) {
     Serial.println("Outside abs. hum > basement abs. hum.");
-    run = false; 
+    run = false;
   }
 
   if (SENS_OUTSIDE.temperature > BASEMENT_TEMP_MAX) {
@@ -326,7 +334,7 @@ bool run_ventilation() {
   if (SENS_OUTSIDE.temperature < BASEMENT_TEMP_MIN) {
     Serial.print("Outside temp is lower then basement min. temp: ");
     Serial.print(BASEMENT_TEMP_MIN);
-    Serial.println(".");    
+    Serial.println(".");
     run = false;
   }
 
@@ -357,9 +365,9 @@ void float2char(float number, char *output, bool sign=false) {
   if (number>=0) {
     dtostrf(number, 4, 1, str_temp);
     if (sign) {
-      sprintf(output, "+%s", str_temp);      
+      sprintf(output, "+%s", str_temp);
     } else {
-      sprintf(output, "%s", str_temp);      
+      sprintf(output, "%s", str_temp);
     }
   }
   else {
@@ -371,113 +379,113 @@ void float2char(float number, char *output, bool sign=false) {
 void pageTemperature(){
   char outside_temp_str[5+1]; // +99.9
   char basement_temp_str[5+1];
-  
+
   //float2char(sensors.temperature, outside_temp_str);
   float2char(SENS_OUTSIDE.temperature, outside_temp_str, true);
   //float2char(-99.9, outside_temp_str);
-  
+
   //char outside_temp[] = dtostrf(myFloat, 1, 4, buffer);String(outside_temp);
   //char inside_temp[] = "-15.2";
   float2char(SENS_BASEMENT.temperature, basement_temp_str, true);
-    
+
   u8g2.drawXBMP( 0, 16, temperature_width, temperature_height, temperature_bits);
 
   u8g2.setFont(u8g2_font_logisoso24_tn);
-  u8g2.drawStr( right_x(outside_temp_str,32), 26, outside_temp_str); 
-  u8g2.drawStr( right_x(basement_temp_str,32), 62, basement_temp_str); 
-  
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(104, 36, "°C");  
-  
-  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);  
-  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);  
+  u8g2.drawStr( right_x(outside_temp_str,32), 26, outside_temp_str);
+  u8g2.drawStr( right_x(basement_temp_str,32), 62, basement_temp_str);
+
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(104, 36, "°C");
+
+  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);
+  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);
   return 0;
 }
-void pageHumidity() { 
+void pageHumidity() {
   char outside_hum[6+1];  // 100.0
   char basement_hum[6+1];  // 100.0
-  
+
   //itoa(round(sensors.humidity), outside_hum, 10);
   float2char(SENS_OUTSIDE.humidity, outside_hum);
   float2char(SENS_BASEMENT.humidity, basement_hum);
-  
+
 
   u8g2.drawXBMP( 0, 16, humidity_32_width, humidity_32_height, humidity_32_bits);
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(8, 64, "RH");    
-  
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(8, 64, "RH");
+
   u8g2.setFont(u8g2_font_logisoso24_tr);
-  u8g2.drawStr(right_x(outside_hum, 32), 26, outside_hum); 
-  u8g2.drawStr(right_x(basement_hum, 32), 64, basement_hum); 
-  
-  
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(104, 36, "%");  
-  
-  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);  
-  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);    
+  u8g2.drawStr(right_x(outside_hum, 32), 26, outside_hum);
+  u8g2.drawStr(right_x(basement_hum, 32), 64, basement_hum);
+
+
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(104, 36, "%");
+
+  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);
+  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);
   return 0;
 }
 
-void pageAbsHumidity() { 
+void pageAbsHumidity() {
   char outside_hum[4+1]; // 99.9
   char basement_hum[4+1]; // 99.9
-  
+
   //itoa(round(rel2abs_hum(sensors.humidity, sensors.temperature)), outside_hum, 10);
   float2char(rel2abs_hum(SENS_OUTSIDE.humidity, SENS_OUTSIDE.temperature ), outside_hum);
   float2char(rel2abs_hum(SENS_BASEMENT.humidity, SENS_BASEMENT.temperature), basement_hum);
 
   u8g2.drawXBMP( 0, 16, humidity_abs_32_width, humidity_abs_32_height, humidity_abs_32_bits);
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(8, 64, "AH");      
-  
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(8, 64, "AH");
+
   u8g2.setFont(u8g2_font_logisoso24_tr);
-  u8g2.drawStr(right_x(outside_hum, 32), 26, outside_hum); 
-  u8g2.drawStr(right_x(basement_hum, 32), 64, basement_hum); 
-  
-  
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(104, 28, "g/"); 
-  u8g2.drawUTF8(104, 44, "m3"); 
-  
-  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);  
-  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);    
+  u8g2.drawStr(right_x(outside_hum, 32), 26, outside_hum);
+  u8g2.drawStr(right_x(basement_hum, 32), 64, basement_hum);
+
+
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(104, 28, "g/");
+  u8g2.drawUTF8(104, 44, "m3");
+
+  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);
+  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);
   return 0;
 }
 
-void pagePressure() {  
+void pagePressure() {
   char outside_press[3+1];
   char basement_press[3+1];
-  
+
   itoa(round(hpa2mmhg(SENS_OUTSIDE.pressure)), outside_press, 10);
   itoa(round(hpa2mmhg(SENS_BASEMENT.pressure)), basement_press, 10);
 
   u8g2.drawXBMP( 0, 16, pressure_32_width, pressure_32_height, pressure_32_bits);
-  
-  u8g2.setFont(u8g2_font_logisoso24_tn);  
 
-  u8g2.drawStr(right_x(outside_press, 32), 24, outside_press); 
-  u8g2.drawStr(right_x(basement_press, 32), 64, basement_press); 
+  u8g2.setFont(u8g2_font_logisoso24_tn);
 
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(104, 28, "mm"); 
-  u8g2.drawUTF8(104, 44, "Hg"); 
-  
-  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);  
-  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);    
+  u8g2.drawStr(right_x(outside_press, 32), 24, outside_press);
+  u8g2.drawStr(right_x(basement_press, 32), 64, basement_press);
+
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(104, 28, "mm");
+  u8g2.drawUTF8(104, 44, "Hg");
+
+  u8g2.drawXBMP( 120, 0, arrow_outside_8_width, arrow_outside_8_height, arrow_outside_8_bits);
+  u8g2.drawXBMP( 120, 56, arrow_inside_8_width, arrow_inside_8_height, arrow_inside_8_bits);
   return 0;
 }
 
 void pageTimeDay() {
   char time_now[] = "08:48";
   char date_now[] = "22.07.18";
-  
-  u8g2.setFont(u8g2_font_inb30_mn);  
-  u8g2.drawStr( center_x(time_now), 40, time_now); 
-  
-  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);  
-  u8g2.drawUTF8(center_x(date_now), 64, date_now);  
-  return 0;  
+
+  u8g2.setFont(u8g2_font_inb30_mn);
+  u8g2.drawStr( center_x(time_now), 40, time_now);
+
+  u8g2.setFont(u8g2_font_pxplusibmvga8_mf);
+  u8g2.drawUTF8(center_x(date_now), 64, date_now);
+  return 0;
 }
 
 unsigned int center_x(char* message, int margin_left=0, int margin_right=0) {
@@ -494,33 +502,33 @@ unsigned int right_x(char* message, int margin_right=0) {
 void blink_fan() {
   /*
    * Blink fan icon.
-   */   
+   */
   if (millis()-Timer_Blink < BLINK_DUR) {
     u8g2.drawXBMP(0, 0, fan_8_width, fan_8_height, fan_8_bits);
   }
   if (millis()-Timer_Blink >= BLINK_DUR && millis()-Timer_Blink < BLINK_DUR*2) {
-    //u8g2.drawXBMP(0, 0, fan_8_width, fan_8_height, fan_8_bits);        
-  }      
-  
-  if (millis()-Timer_Blink >= BLINK_DUR*2) {
-    Timer_Blink = millis();  
+    //u8g2.drawXBMP(0, 0, fan_8_width, fan_8_height, fan_8_bits);
   }
-}      
+
+  if (millis()-Timer_Blink >= BLINK_DUR*2) {
+    Timer_Blink = millis();
+  }
+}
 
 void blink_receiving() {
   /*
    * Blink receiving icon.
-   */   
+   */
   if (millis()-Timer_Receiving < BLINK_DUR/2) {
     u8g2.drawXBMP(8, 0, arrow_receive_8_width, arrow_receive_8_height, arrow_receive_8_bits);
-    u8g2.setFont(u8g2_font_profont12_mn);  
+    u8g2.setFont(u8g2_font_profont12_mn);
     u8g2.setCursor(16, 8);
     u8g2.print(Receiving_From_Node);
   } else {
     Receiving = false;
-    //Timer_Receiving = millis();  
+    //Timer_Receiving = millis();
   }
-}      
+}
 
 
 bool Node_Outside_Active() {
@@ -528,7 +536,7 @@ bool Node_Outside_Active() {
    * Check if the Noda data are up to date.
    */
   if (millis()-Timer_Node_Outside >= Node_Timeout) {
-    return false;     
+    return false;
   } else {
     return true;
   }
@@ -539,39 +547,70 @@ bool Node_Basement_Active() {
    * Check if the Noda data are up to date.
    */
   if (millis()-Timer_Node_Basement >= Node_Timeout) {
-    return false; 
+    return false;
   } else {
     return true;
-  }  
+  }
 }
 
-void loop() { 
-  
+void oled_display_state(bool turn_on=true) {
+  /*
+  * Turns OLED display on or off.
+  */
+  if (turn_on) {
+    u8g2.setPowerSave(false);
+    Timer_Display_On = millis();
+    Display_On = true;
+  } else {
+    u8g2.setPowerSave(true);
+    Display_On = false;
+  }
+}
+
+void oled_display_on(bool state=false) {
+  /*
+  * Turn OLED display on.
+  */
+  oled_display_state(true);
+}
+
+void oled_display_off () {
+  /*
+  * Turn OLED display off.
+  */
+  oled_display_state(false);
+}
+
+
+void loop() {
+
   // wireless updates
   bus.receive(50000);
   bus.update();
 
-  u8g2.clearBuffer(); 
+  u8g2.clearBuffer();
 
   if (Receiving) {
-    //blink_fan();  
+    //blink_fan();
     blink_receiving();
   }
 
   if (Fan_On) {
     blink_fan();
   }
-      
-  (*pages[current_page])();
-  u8g2.sendBuffer();      
-  
-  if (millis()-Timer_Page_Display >= page_duration[current_page]) {
-    current_page++;  
-    if (current_page > pages_count-1)
-      current_page=0;    
 
-    Timer_Page_Display = millis();
-  }  
+  if (Display_On) {
+      (*pages[current_page])();
+      u8g2.sendBuffer();
+
+    if (millis()-Timer_Page_Display >= page_duration[current_page]) {
+      current_page++;
+      if (current_page > pages_count-1)
+        current_page=0;
+
+      Timer_Page_Display = millis();
+    }
+  }
 
   // check if we need to run vetilation
   if (time_to_check_vent()) {
@@ -579,13 +618,25 @@ void loop() {
       Timer_Vent_Running = millis(); // reset counter
       // send signal to run
       CONTROL Command = {"fan","on",0};
-      bus.send(NODE_ID_BASEMENT, (const char*)&Command, sizeof(CONTROL));   
+      bus.send(NODE_ID_BASEMENT, (const char*)&Command, sizeof(CONTROL));
       Fan_On = true;
     } else {
       CONTROL Command = {"fan","off",0};
-      bus.send(NODE_ID_BASEMENT, (const char*)&Command, sizeof(CONTROL));         
+      bus.send(NODE_ID_BASEMENT, (const char*)&Command, sizeof(CONTROL));
       Fan_On = false;
-    } 
+    }
   }
-  
+
+  if (millis()-Timer_Display_On >= DISPLAY_SAVER && Display_On) {
+    oled_display_off();
+  }
+
+  if (!Display_On) {
+    if (digitalRead(PROXIMITY_PIN) == LOW) {
+      current_page=0; // start displaying pages from the begining
+      Timer_Page_Display = millis(); // reset current page time
+      oled_display_on();
+    }
+  }
+
 };
